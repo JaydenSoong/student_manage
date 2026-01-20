a5e6aa9e13a3c4f185a5a7afb9525a3e287ec69d
JaydenSoong <jiandongsong@126.com>
2026-01-20 16:52:34 +0800

    创建ListView列表视图类

---
+++ grades/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import GradeListView
+
+urlpatterns = [
+    path('list/', GradeListView.as_view()),
+]
--- grades/views.py
+++ grades/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Grade
 
 # Create your views here.
+class GradeListView(ListView):
+    # 指定模型
+    model = Grade
+    # 指定模板
+    template_name = 'grades/list.html'
+    # 显示字段
+    fields = ['grade_name', 'grade_number']
+    # 返回的数据
+    context_object_name = 'grades'

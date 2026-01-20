816903334d44a197f1f4857e84290a0aad36931e
JaydenSoong <jiandongsong@126.com>
2026-01-20 16:28:34 +0800

    创建Grade模型并配置后台

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'grades',
+    'scores',
+    'students',
+    'teachers',
+    'accounts'
 ]
 
 MIDDLEWARE = [

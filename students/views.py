4c95b0e2a35f794d8433c1c85ad9f4425de712cc
JaydenSoong <jiandongsong@126.com>
2026-01-22 18:17:02 +0800

    完成批量删除学生功能

--- students/views.py
+++ students/views.py
@@ -117,3 +117,32 @@ class StudentDeleteView(UpdateView):
                 'status': 'error',
                 'message': '删除失败' + str(e)
             }, status=500)
+
+class StudentBulkDeleteView(DeleteView):
+    model =  Student
+    success_url = reverse_lazy('student_list')
+
+    def post(self, request, *args, **kwargs):
+        student_ids = request.POST.getlist('student_ids')
+        print(student_ids)
+        if not student_ids:
+            return JsonResponse({
+                'status': 'error',
+                'message': '请选择要删除的学生'
+            }, status=400)
+        self.object_list = self.get_queryset().filter(id__in=student_ids)
+        try:
+            for student in self.object_list:
+                # 删除关联的 user 表中的数据
+                student.user.delete()
+                # 删除 student 表中的数据
+            self.object_list.delete()
+            return JsonResponse({
+                'status': 'success',
+                'message': '删除成功'
+            }, status=200)
+        except Exception as e:
+            return JsonResponse({
+                'status': 'error',
+                'message': '删除失败' + str(e)
+            }, status=500)
